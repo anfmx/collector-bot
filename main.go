c9751f7eeaf9799d6861eb22bd86227f14ed011c
Anfmx <vlad.anfimov.1997@gmail.com>
2025-01-06 17:19:19 +0300

    some changes

---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	tgClient "TelegramBot/clients/telegram"
+	event_consumer "TelegramBot/consumer/event-consumer"
+	"TelegramBot/events/telegram"
+	"TelegramBot/storage/files"
+	"flag"
+	"log"
+)
+
+const (
+	tgBotHost   = "api.telegram.org"
+	storagePath = "files_storage"
+	batchSize   = 100
+)
+
+func main() {
+
+	eventsProcessor := telegram.New(
+		tgClient.New(tgBotHost, mustToken()),
+		files.New(storagePath),
+	)
+
+	log.Print("Service started")
+
+	consumer := event_consumer.New(eventsProcessor, eventsProcessor, batchSize)
+
+	if err := consumer.Start(); err != nil {
+		log.Fatal("service is stopped", err)
+	}
+}
+
+func mustToken() string {
+	token := flag.String("tg-bot-token",
+		"",
+		"token for access to telegram bot")
+	flag.Parse()
+	if *token == "" {
+		log.Fatal("token isn't specified")
+	}
+	return *token
+}
